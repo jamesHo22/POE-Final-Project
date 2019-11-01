65d65067720e9dc910731f536ad00d13cf109863
jamesho <8.james.ho@gmail.com>
2019-11-01 12:09:24 -0400

    init commit

---
+++ app.py
@@ -0,0 +1,19 @@
+# Tutorial from https://flask-socketio.readthedocs.io/en/latest/
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@app.route('/')
+def hello():
+    return render_template('main.html')
+
+# Handle messages from client
+@socketio.on('yeetmessage')
+def handle_message(message):
+    print(f'received message: {message}')
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, host= '0.0.0.0')
