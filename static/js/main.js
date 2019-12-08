ebf68c4b131b95e81b2394d365e81b51744ba4f8
jamesho <8.james.ho@gmail.com>
2019-12-08 14:26:08 -0500

    going to use subplots instead

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,5 @@
 
-let socket = io.connect('https://' + document.domain + ':' + location.port);
+let socket = io.connect('http://' + document.domain + ':' + location.port);
 console.log('https://' + document.domain + ':' + location.port)
 socket.on('connect', function() {
     socket.emit('my event', {data: 'User connected!'});
